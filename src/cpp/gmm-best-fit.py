7d244ecac8ea6d5c220989ed7b9d33490976de06
patrick <patrick.cmorneau@cidco.ca>
2022-04-21 15:21:57 -0400

    print point with class

--- src/cpp/gmm-best-fit.py
+++ src/cpp/gmm-best-fit.py
@@ -13,11 +13,12 @@ from sklearn import mixture
 from sklearn.model_selection import train_test_split
 #from sklearn import metrics
 
-if len(sys.argv) != 2:
-	sys.stderr.write("Usage: gmm-best-fit.py training-data.txt\n")
+if len(sys.argv) != 3:
+	sys.stderr.write("Usage: gmm-best-fit.py training-data.txt nb-max-class \n")
 	sys.exit(1)
 
 trainingFile = sys.argv[1]
+maxClusters = int(sys.argv[2])
 
 #Load training data
 with open(trainingFile) as f:
@@ -28,25 +29,12 @@ with open(trainingFile) as f:
 
 	features = []
 	labels = []
-
-	#Parse features and labels
+	
 	for x in labeled_data:
 		y = [ float(h) for h in x[3:-2]]
 		features.append( y )
-		labels.append( x[-2] )
-
-
-	#Preprocess labels
-	labelEncoder = preprocessing.LabelEncoder()
-	labels_encoded = labelEncoder.fit_transform(labels)
-
-	labelDictionary = dict(zip(labelEncoder.transform(labelEncoder.classes_),labelEncoder.classes_))
-
-	featuresTrain, featuresTest, labelsTrain, labelsTest = train_test_split(features,labels_encoded, test_size=0.2,random_state=109) #80/20 split
 	
-	#print(features)
-	
-	#features = np.array(features)
+	features = np.array(features)
 	bic = []
 	lowest_bic = np.infty
 	
@@ -56,29 +44,37 @@ with open(trainingFile) as f:
 		print(n)
 		model = mixture.GaussianMixture(n, covariance_type = "full")
 		n += 1
-		model.fit(features,labelsTrain)
-		bic.append(model.bic(np.array(featuresTrain)))
+		model.fit(features)
+		bic.append(model.bic(features))
 		if bic[-1] < lowest_bic:
 			lowest_bic = bic[-1]
 			best_model = model
 		else:
 			break;
-		if n > 101:
+		if n > maxClusters:
 			break;
 
 	
-	print(bic)
-	print(min(bic))
+	#print(bic)
+	print("bic min: ", min(bic))
 	bestFit = 1 + bic.index(min(bic))
-	print(bestFit)
+	print("best fit: ", bestFit)
 	
-	predictions = model.predict(featuresTest)
+	predictions = model.predict(features)
 	print(predictions)
-	print(labelsTest)
 	
-	
-	y = np .arange(len(bic))
+	for i in range(len(features)):
+		xyz = features[i][:3]
+		klass = predictions[i]
+		print(str(xyz[0]),str(xyz[1]),str(xyz[2]) , klass)
+
+	y = np.arange(1, len(bic)+1)
 	x = np.array(bic)
+	plt.gca().invert_yaxis()
+	plt.gca().invert_xaxis()
 	plt.plot(x,y)
+	plt.xlabel("best fit")
+	plt.xlabel("N clusters")
+	plt.ylabel("BIC")
 	plt.show()
 
