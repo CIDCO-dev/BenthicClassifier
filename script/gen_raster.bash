f50d0ffff59a357e4b4af3b565e1614de46d2cd9
pat <patrick.cmorneau@cidco.ca>
2024-01-10 12:08:20 -0500

    batch map generation script + fix

--- script/gen_raster.bash
+++ script/gen_raster.bash
@@ -1,94 +1,25 @@
 #!/bin/bash
 
-gen_vrt_gbx(){
-	classificationsDir=$1
-	xyzFile=$2
-	vrtDir=$3
-	vrtFilename=$4
-#	echo "1 " $xyzFile
-#	echo "2 " ${vrtFilename%.*}
-	output=$vrtDir/$vrtFilename
-	echo "create virtual file format = " $output
-
-
-	echo "<OGRVRTDataSource>
-		<OGRVRTLayer name=\"${vrtFilename%.*}\">
-		    <SrcDataSource>$classificationsDir/$xyzFile</SrcDataSource>
-		    <GeometryType>wkbPoint</GeometryType>
-		    <GeometryField encoding=\"PointFromColumns\" x=\"x\" y=\"y\" z=\"boosting class\"/>
-		</OGRVRTLayer>
-	</OGRVRTDataSource>" > $output
-	
-}
-
-gen_vrt_gmm(){
-	classificationsDir=$1
-	xyzFile=$2
-	vrtDir=$3
-	vrtFilename=$4
-	echo "1 " $xyzFile
-	echo "2 " ${vrtFilename%.*}
-	output=$vrtDir/$vrtFilename
-	echo "create virtual file format = " $output
-
-
-	echo "<OGRVRTDataSource>
-		<OGRVRTLayer name=\"${vrtFilename%.*}\">
-		    <SrcDataSource>$classificationsDir/$xyzFile</SrcDataSource>
-		    <GeometryType>wkbPoint</GeometryType>
-		    <GeometryField encoding=\"PointFromColumns\" x=\"x\" y=\"y\" z=\"gmm class\"/>
-		</OGRVRTLayer>
-	</OGRVRTDataSource>" > $output
-	
-}
-
-if [[ $# -ne 4 ]]; then
-	echo "usage : bash gen_raster.bash classificationsDirectory resolutionX resolutionY epsgCode"
-	exit 1
-fi
-
-if [[ "$(basename $(pwd))" != "BenthicClassifier" ]]; then
+if [[ "$(basename "$PWD")" != "BenthicClassifier" ]]; then
 	echo "please execute script from project root"
 	exit 1
 fi
 
-
+if [[ $# -ne 2 ]]; then
+	echo "usage : bash hackel_batch.bash directory outputDirectory"
+	exit 1
+fi
 
 dir=$1
-vrtGbxDir=$dir/vrts_gbx
-vrtGmmDir=$dir/vrts_gmm
-rasterGbxDir=$dir/rasters_gbx
-rasterGmmDir=$dir/rasters_gmm
-mkdir -p $rasterGbxDir
-mkdir -p $rasterGmmDir
-mkdir -p $vrtGbxDir
-mkdir -p $vrtGmmDir
+outDir=$2
 
-resolutionX=$2
-resolutionY=$3
-epsg=$4
+mkdir -p $outDir
 
-# gbx
 for FILE in $(ls $dir/*.csv);
 do
-	FILE=$(basename "$FILE")
-	vrtFilename=${FILE%.*}.vrt
-	echo $vrtFilename
-	gen_vrt_gbx $dir $FILE $vrtGbxDir $vrtFilename
-	gdal_grid -outsize $resolutionX $resolutionY -a_srs EPSG:$epsg -a invdist -of GTiff -ot Float64 -l ${vrtFilename%.*} $vrtGbxDir/$vrtFilename $rasterGbxDir/$FILE.tiff --config GDAL_NUM_THREADS ALL_CPUS
-
-done
-
-
-# gmm
-for FILE in $(ls $dir/*.csv);
-do
-	FILE=$(basename "$FILE")
-	vrtFilename=${FILE%.*}.vrt
 	echo $FILE
-	gen_vrt_gmm $dir $FILE $vrtGmmDir $vrtFilename
-	gdal_grid -outsize $resolutionX $resolutionY -a_srs EPSG:$epsg -a invdist -of GTiff -ot Float64 -l ${vrtFilename%.*} $vrtGmmDir/$vrtFilename $rasterGmmDir/$FILE.tiff --config GDAL_NUM_THREADS ALL_CPUS
-
+	FILENAME=$(basename -s .csv $FILE)
+	python3 src/generate_raster_map.py $FILE $outDir/$FILENAME.tif $FILENAME
 done
 
 
--- src/generate_raster_map.py
+++ src/generate_raster_map.py
@@ -6,12 +6,13 @@ import pandas as pd
 colormap = {0:"red", 1:"grey", 2:"blue", 3:"pink", 4:"orange", 5:"black", 6:"green", 7:"purple", 8:"yellow", 9:"brown"}
 gbxClassMap = {0:"blocks", 1:"cobble", 2:"gravels", 3:"rocky", 4:"sand", 5:"sandy mud"}
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
 	sys.stderr.write("Usage: python3 generate_raster_map.py inputData Map_title\n")
 	sys.exit(1)
 	
 inputFilePath = sys.argv[1]
-mapTitle = sys.argv[2]
+outputFilename = sys.argv[2]
+mapTitle = sys.argv[3]
 
 
 data = pd.read_csv(inputFilePath, delimiter=",", dtype=float, header=0)
@@ -23,20 +24,20 @@ minLon = data.min(axis=0)[1]
 maxLat = data.max(axis=0)[0]
 maxLon = data.max(axis=0)[1]
 
-gbxGroups = data.groupby("class")
-#gmmGroups = data.groupby("gmmClass")
+gbxGroups = data.groupby("boosting class")
 
 
 fig = pygmt.Figure()
 fig.basemap(region=[minLon - 0.005, maxLon + 0.005, minLat - 0.005, maxLat + 0.005], 
 			frame=["a", "+t{}".format(mapTitle)],
-			projection="M6c"
+			projection="M6i"
 )
-fig.coast(resolution="f", shorelines="thinnest", land="lightgreen", water="lightblue")
+
+fig.coast(land="lightblue", water="lightblue")
 
 for classID in gbxGroups.groups:
 	df = gbxGroups.get_group(classID)
-	fig.plot(x=df["long"], y=df["lat"], style="c0.01c", fill=colormap[classID], label=gbxClassMap[classID]+"+S0.25c")
+	fig.plot(x=df["longitude"], y=df["latitude"], style="c0.03c", fill=colormap[classID], label=gbxClassMap[classID]+"+S0.25c")
 
 
 fig.legend(box=True)
@@ -46,21 +47,16 @@ fig.image(
     box=True,
 )
 
-
-
 with fig.inset(
-    position="jBL",
-    region=[round(minLon,3)-1, round(maxLon,3)+1, round(minLat,3)-1, round(maxLat,3)+1],
-    projection="M2c",
-    box="+gwhite+p1p"
+	position="jBL",
+	region=[round(minLon-0.3,3), round(maxLon+0.3,3), round(minLat-0.3,3), round(maxLat+0.3,3)],
+	box="+gwhite+p1p",
+	projection="M2c"
 ):
-	fig.coast(
-		shorelines="thinnest", land="grey", water="lightblue"
-	)
+	fig.coast(shorelines="thinnest", land="green", water="lightblue")
 	fig.plot(data=[[minLon, minLat, maxLon, maxLat]], style="r+s", pen="1p,red")
 
-
-fig.show()
-
+#fig.show()
+fig.savefig("{}".format(outputFilename), crop=True, dpi=1200)
 
 
